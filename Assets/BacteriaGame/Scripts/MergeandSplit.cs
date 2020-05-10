d94552889401181809d83787478a76b83aaa0014
Scott Duff <duff.scott.richard@gmail.com>
2020-05-10 15:00:08 -0400

    Gamemanager created with win and loss conditions

--- Assets/BacteriaGame/Scripts/MergeandSplit.cs
+++ Assets/BacteriaGame/Scripts/MergeandSplit.cs
@@ -7,27 +7,9 @@ public class MergeandSplit : MonoBehaviour
 {
     
     public float radius;
-    public float maxDistance;
-    public LayerMask layerMask;
-
-    public GameObject currentHitObject;
-
-
-
-    public int thing;
-
-
-   
     public List<Collider> blobAmount = new List<Collider>();
     private int sizeOfList;
 
-    Collider[] blobsInsideDecetionZone;
-    Collider[] blobsOutsideDecetionZone;
-
-    private Vector3 origin;
-    
-
-    private float currentHitDistance;
 
 
     private void Update()
@@ -44,78 +26,19 @@ public class MergeandSplit : MonoBehaviour
 
     void DetectBlobs()
     {
-        blobsInsideDecetionZone = Physics.OverlapSphere(transform.position, radius, layerMask);
-
-        foreach(var blob in blobsInsideDecetionZone)
+        if (Input.GetKeyDown(KeyCode.Space) && sizeOfList >= 2)
         {
-            
-            if (!blobAmount.Contains(blob))
-            {
-                blobAmount.Add(blob);
-            }
+            Debug.Log("Pressing SPACE BAR");
+            GetComponent<MeshRenderer>().enabled = false;
 
-
-            //cubesOutsideZone = blobAmount.Except
         }
 
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            GetComponent<MeshRenderer>().enabled = true;
 
+        }
 
-
-
-
-
-
-
-
-
-
-
-        //origin = transform.position;
-        //direction = transform.forward;
-        //RaycastHit hit;
-
-        //if(Physics.SphereCast(origin, radius, Vector3.forward, out hit, maxDistance, layerMask, QueryTriggerInteraction.UseGlobal))
-        //{
-        //    currentHitObject = hit.transform.gameObject;
-        //    currentHitDistance = hit.distance;
-
-        //}
-        //else
-        //{
-        //    currentHitDistance = maxDistance;
-        //    currentHitObject = null;
-        //}
-
-
-
-
-
-
-
-
-
-
-
-        //if(Input.GetKeyDown(KeyCode.Space) && sizeOfList >= 2)
-        //{
-        //    Debug.Log("Pressing SPACE BAR");
-        //    GetComponent<MeshRenderer>().enabled = false;
-
-        //}
-
-        //if(Input.GetKeyDown(KeyCode.Escape))
-        //{
-        //    GetComponent<MeshRenderer>().enabled = true;
-
-        //}
-     
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Debug.DrawLine(origin, origin + Vector3.zero * currentHitDistance);
-        Gizmos.DrawWireSphere(origin + Vector3.zero * currentHitDistance, radius);
     }
 
     private void OnTriggerEnter(Collider other)
