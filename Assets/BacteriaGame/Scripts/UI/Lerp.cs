701c8d20505b89f1b098a31cc1014723b393cfd2
darrylfeniquito <48287066+darrylfeniquito@users.noreply.github.com>
2020-05-10 14:08:40 -0400

    mainmenu
    mainmenu work in progress

---
+++ Assets/BacteriaGame/Scripts/UI/Lerp.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lerp : MonoBehaviour
+{
+    
+    public float minimum = -1.0F;
+    public float maximum = 1.0F;
+
+    // starting value for the Lerp
+    static float t = 0.0f;
+
+    void Update()
+    {
+        // animate the position of the game object...
+        transform.position = new Vector3(transform.position.x, Mathf.Lerp(minimum, maximum, t), transform.position.z);
+
+        // .. and increase the t interpolater
+        t += 0.4f * Time.deltaTime;
+
+      
+        if (t > 1.0f)
+        {
+            float temp = maximum;
+            maximum = minimum;
+            minimum = temp;
+            t = 0.0f;
+        }
+    }
+}
+
