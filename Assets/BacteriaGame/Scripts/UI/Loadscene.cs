9c55ac99550111dbbcfa654af535b3a1768a9371
darrylfeniquito <48287066+darrylfeniquito@users.noreply.github.com>
2020-05-10 14:15:44 -0400

    scenemanager to maingame
    scenemanager to maingame. just need to merge branches and comment the code back in.

--- Assets/BacteriaGame/Scripts/UI/Loadscene.cs
+++ Assets/BacteriaGame/Scripts/UI/Loadscene.cs
@@ -7,7 +7,7 @@ public class Loadscene : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //SceneManager.LoadScene();
+        //SceneManager.LoadScene(MainGame);
     }
 
     // Update is called once per frame
