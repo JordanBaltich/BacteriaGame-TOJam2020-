65c34fe46f33a3fd7d8c7b8efc4900120b9f06f7
darrylfeniquito <48287066+darrylfeniquito@users.noreply.github.com>
2020-05-10 15:33:20 -0400

    loadscene work
    mainmenu to maingame works. Text for win/lose states also implemented to maingame UI canvas.

--- Assets/BacteriaGame/Scripts/UI/Loadscene.cs
+++ Assets/BacteriaGame/Scripts/UI/Loadscene.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Loadscene : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        //SceneManager.LoadScene(MainGame);
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            SceneManager.LoadScene("MainGame");
+        }
     }
+
 }
